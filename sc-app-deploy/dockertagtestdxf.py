91740050528a57a722505569216554cad28cc24f
ldahlke <lars.dahlke@capgemini.com>
2020-11-23 07:40:55 +0000

    Purge

--- sc-app-deploy/dockertagtestdxf.py
+++
@@ -1,9 +0,0 @@
-from dxf import DXF
-
-def auth(dxf, response):
-    dxf.authenticate('schulclouddevops', 'jimeeCh3ei', response=response)
-
-dxf = DXF('registry-1.docker.io', 'schulcloud/schulcloud-server', auth)
-
-tag_exist = dxf.get_manifest('develop_latest')
-print ("%b" % tag_exist)
